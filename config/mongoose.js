33c5bde49c0f42634cd74b67e104d2b5c70cc5eb
Ashish bundela <ashishbundela123@gmail.com>
2023-06-13 18:59:48 +0530

    Commit message

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
-const mongoDB = "mongodb://127.0.0.1:27017/placement4"; 
+const mongoDB = "mongodb+srv://ashishbundela123:GBZerteAAf2lzu3Z@cluster0.enidn78.mongodb.net/"; 
 main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
   console.log('connected to mongoDB');
 }
+
+
